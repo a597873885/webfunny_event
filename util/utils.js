10b90ac2ce763723e03d7aaa8dee63afa32521da
yulei <960515179@qq.com>
2021-10-19 22:35:52 +0800

    修复VIP激活码过期问题

--- util/utils.js
+++ util/utils.js
@@ -160,6 +160,15 @@ const Utils = {
       return encryptString
     }
   },
+  md5Hex: function(encryptString) {
+    try {
+      let hash = crypto.createHash('md5');
+      return hash.update(encryptString).digest('hex');
+    } catch(e) {
+      console.log(e)
+      return encryptString
+    }
+  },
   setTableName(name, day) {
     return name + new Date(new Date().getTime() + (86400000 * (day))).Format("yyyyMMdd")
   },
